2b8ca9097648eb022af8d6d0faf3033fafd21b0f
Данила Семешкин <semyoshkin8v@mail.ru>
2021-11-30 21:08:21 +0300

    add NumericUpDown

---
+++ Semeshkin.RPKS.WPF.MVVM/ViewModels/NumericUpDownViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Semeshkin.WPF.MVVM.Core.Command;
+using Semeshkin.WPF.MVVM.Core.ViewModel;
+using Semeshkin.WPF.MVVM.Data;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Semeshkin.RPKS.WPF.MVVM.ViewModels
+{
+    internal sealed class NumericUpDownViewModel : ViewModelBase
+    {
+        #region Fields
+
+        private ICommand _incrementStepCommand;
+        private ICommand _decrementStepCommand;
+
+        #endregion
+
+        #region Constructors
+
+        public NumericUpDownViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ICommand IncrementStepCommand => _incrementStepCommand ??= new RelayCommand(tb => IncrementFunc(tb as TextBox), tb => CheckForExecute(tb as TextBox));
+        public ICommand DecrementStepCommand => _decrementStepCommand ??= new RelayCommand(tb => DecrementFunc(tb as TextBox), tb => CheckForExecute(tb as TextBox));
+
+        #endregion
+
+        #region Methods
+
+        private void IncrementFunc(TextBox tb)
+        {
+            tb.Text = (int.Parse(tb.Text) + 1).ToString();
+        }
+
+        private void DecrementFunc(TextBox tb)
+        {
+            tb.Text = (int.Parse(tb.Text) - 1).ToString();
+        }
+
+        private bool CheckForExecute(TextBox tb)
+        {
+            return int.TryParse(tb.Text, out _);
+            //return true;
+        }
+
+        #endregion
+    }
+}
--- Semeshkin.Wpf.Controls/NumericUpDown.xaml.cs
+++ Semeshkin.Wpf.Controls/NumericUpDown.xaml.cs
@@ -20,5 +20,126 @@ namespace Semeshkin.Wpf.Controls
         {
             InitializeComponent();
         }
+
+        static NumericUpDown()
+        {
+            ValueProperty = DependencyProperty.Register(
+                nameof(Value),
+                typeof(int),
+                typeof(NumericUpDown),
+                new PropertyMetadata(default(int), (d, e) => { },
+                (d, value) =>
+                {
+                    if (!(d is NumericUpDown userControl))
+                    {
+                        throw new ArgumentException($"Parameter \"{d}\") should be of type \"{typeof(NumericUpDown).FullName}\"");
+                    }
+
+                    if (!(value is int valueInt))
+                    {
+                        throw new ArgumentException($"Parameter \"{value}\") should be of type \"{typeof(int).FullName}\"");
+                    }
+
+                    return valueInt < userControl.MinValue
+                        ? userControl.MinValue
+                        : valueInt > userControl.MaxValue ? userControl.MaxValue : (object)valueInt;
+                }), value =>
+                {
+                    return !(value is int valueInt)
+                        ? throw new ArgumentException($"Parameter \"{value}\") should be of type \"{typeof(int).FullName}\"")
+                        : true;
+                });
+
+            MinValueProperty = DependencyProperty.Register(
+                nameof(MinValue),
+                typeof(int),
+                typeof(NumericUpDown),
+                new PropertyMetadata(int.MinValue, (d, e) => { },
+                (d, value) =>
+                {
+                    if (!(d is NumericUpDown userControl))
+                    {
+                        throw new ArgumentException($"Parameter \"{d}\") should be of type \"{typeof(NumericUpDown).FullName}\"");
+                    }
+
+                    if (!(value is int valueInt))
+                    {
+                        throw new ArgumentException($"Parameter \"{value}\") should be of type \"{typeof(int).FullName}\"");
+                    }
+
+                    return valueInt > userControl.MaxValue
+                        ? int.MinValue
+                        : valueInt;
+                }));
+
+            MaxValueProperty = DependencyProperty.Register(
+                nameof(MaxValue),
+                typeof(int),
+                typeof(NumericUpDown),
+                new PropertyMetadata(int.MaxValue, (d, e) => { },
+                (d, value) =>
+                {
+                    if (!(d is NumericUpDown userControl))
+                    {
+                        throw new ArgumentException($"Parameter \"{d}\") should be of type \"{typeof(NumericUpDown).FullName}\"");
+                    }
+
+                    if (!(value is int valueInt))
+                    {
+                        throw new ArgumentException($"Parameter \"{value}\") should be of type \"{typeof(int).FullName}\"");
+                    }
+
+                    return valueInt < userControl.MinValue
+                        ? int.MaxValue
+                        : valueInt;
+                }));
+
+            IncrementStepCommandProperty = DependencyProperty.Register(
+                nameof(IncrementStepCommand),
+                typeof(ICommand),
+                typeof(NumericUpDown));
+
+            DecrementStepCommandProperty = DependencyProperty.Register(
+                nameof(DecrementStepCommand),
+                typeof(ICommand),
+                typeof(NumericUpDown));
+        }
+
+        public static readonly DependencyProperty ValueProperty;
+        public static readonly DependencyProperty MinValueProperty;
+        public static readonly DependencyProperty MaxValueProperty;
+        public static readonly DependencyProperty IncrementStepCommandProperty;
+        public static readonly DependencyProperty DecrementStepCommandProperty;
+
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public int MinValue
+        {
+            get => (int)GetValue(MinValueProperty);
+            set => SetValue(MinValueProperty, value);
+        }
+
+        public int MaxValue
+        {
+            get => (int)GetValue(MaxValueProperty);
+            set => SetValue(MaxValueProperty, value);
+        }
+
+        public ICommand IncrementStepCommand
+        {
+            get => (ICommand)GetValue(IncrementStepCommandProperty);
+            set => SetValue(IncrementStepCommandProperty, value);
+        }
+
+        public ICommand DecrementStepCommand
+        {
+            get => (ICommand)GetValue(DecrementStepCommandProperty);
+            set => SetValue(DecrementStepCommandProperty, value);
+        }
     }
 }
